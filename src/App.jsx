5ce449c4d2b1ffb071b96d89f09cbc7e8e4400df
kuldeep <kellysonu21@gmail.com>
2025-09-05 14:41:57 +0530

    initialize git

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import { Outlet } from 'react-router-dom'
 
+// initialize git in my project
+
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch();
